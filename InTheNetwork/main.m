00c90150799fb0086b756c7c179de38695a4bc23
reeonce <reeonce@yahoo.com>
2017-04-28 23:52:43 +0800

    Initial Commit

---
+++ InTheNetwork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InTheNetwork
+//
+//  Created by Ree on 28/04/2017.
+//  Copyright © 2017 reeonce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
